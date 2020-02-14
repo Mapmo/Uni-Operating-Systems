80c5b53643cbdc42ccfee31aba1707e7ad01760d
Iliev <Martin.Iliev@sega.co.uk>
2020-02-14 16:39:28 +0200

    Fix indentation

--- OS_Practice_Homeworks/Homework_4/socket_solution/client.c
+++ OS_Practice_Homeworks/Homework_4/socket_solution/client.c
@@ -57,9 +57,10 @@ void SetMutex()
 
 void ConnectToBank(const int s, struct sockaddr_un* remote)
 {
-  	remote->sun_family = AF_UNIX;
-        strcpy(remote->sun_path, SOCK_PATH);
-        int len = strlen(remote->sun_path) + sizeof(remote->sun_family);
+  	remote -> sun_family = AF_UNIX;
+        strcpy(remote -> sun_path, SOCK_PATH);
+        
+	int len = strlen(remote -> sun_path) + sizeof(remote -> sun_family);
         if (connect(s, (struct sockaddr *) remote, len) == -1)
         {
                 Release(s);
@@ -76,6 +77,7 @@ void ConnectToBank(const int s, struct sockaddr_un* remote)
 void ProcessDataFromBank(const int s, const char ACCOUNT, char* str)
 {
 	int t;
+	
 	if(send(s, &ACCOUNT, 1, 0) == -1)
         {
                 Release(s);
@@ -142,7 +144,8 @@ void ProcessDataFromBank(const int s, const char ACCOUNT, char* str)
                 err(5, "error receiving balance");
         }
         str[t] = '\0';
-        if(printf("%s\n", str) == -1)
+        
+	if(printf("%s\n", str) == -1)
 	{
                 Release(s);
                 err(3, "error printing 'connected'");
@@ -174,7 +177,8 @@ void SendRequest(const int s, char* str)
 void Exit(const int s, const char status)
 {
         Release(s);
-        if(status == 1)
+        
+	if(status == 1)
         {
                 printf("User completed transaction successfully\n");
                 exit(0);
@@ -183,7 +187,6 @@ void Exit(const int s, const char status)
         {
                 errx(6, "Transaction failed\n");
         }
-
 }
 
 int main(const int argc, const char* argv[])
