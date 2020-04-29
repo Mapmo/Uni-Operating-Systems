52013c6302c68036145c0b440013d998af99d2e9
Martin Iliev <mapmoeu@gmail.com>
2020-04-29 23:34:54 +0300

    Adjust the script to my current setup

--- misc/rsync.sh
+++ misc/rsync.sh
@@ -2,5 +2,4 @@
 
 #used for backuping my file system to an external hard drive
 
-sudo rsync -aAXv / --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/timeshift/*","/boot/efi/*","/home/martin/VirtualBox VMs/*","/home/martin/Downloads/*","/home/martin/.cache","/var/tmp/*","/home/martin/.mozilla"} .
-
+sudo rsync -aAXv --delete / --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/boot/efi","/timeshift","/home/mapmo/vms","/home/mapmo/Downloads/*","/home/mapmo/Videos/*","/home/mapmo/.cache/*","/home/mapmo/.local/share/Trash/*","/var/tmp/*"} /mnt
