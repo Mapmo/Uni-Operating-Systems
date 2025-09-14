66380eae80dab7d14670b8c4a56a39cf364a3b70
Martin Iliev <martin.iliev@cognyte.com>
2025-09-14 17:15:57 +0300

    Add new script

---
+++ scripts/deutsch/examine.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import argparse
+import json
+import sys
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser(prog="test")
+    parser.add_argument("filename")
+    parser.add_argument("--reversed", "-r", action="store_true")
+    args = parser.parse_args()
+    try:
+        with open(args.filename) as fd:
+            tests = json.load(fd)
+            if args.reversed:
+                tests = {v: k for k, v in tests.items()}
+            for key in tests.keys():
+                ans = input(key)
+    except FileNotFoundError:
+        print("File not found")
+        return 1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
